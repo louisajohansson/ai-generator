ad4227fda17c696e26a1880508997e03c133f4bb
louisajohansson <louisa.j.johansson1@gmail.com>
2024-11-26 11:33:12 +0100

    Updated user friendliness

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generateAnswer(event) {
     "You are a masterquoter AI assistent who knows all the best quotes in the world. Give the best quote using the users instructions. The quote shouldn't be more than three lines and seperated by <br />. Sign if the name of the person you quoted but their name should be underneat the quote following <br />. No offensive and very sweet and kind quotes";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+let quoteElement = document.querySelector("#form-answer");
+quoteElement.classList.remove("hidden");
+quoteElement.innerHTML = `Generating quote… about ${instructionElement.value}`; 
+
   axios.get(apiUrl).then(displayAnswer);
 }
 
