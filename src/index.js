2b3648e986ffe2aa4222c65b3e71dfd13945427f
louisajohansson <louisa.j.johansson1@gmail.com>
2024-11-26 11:09:37 +0100

    Integrated AI API & axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+function displayAnswer(response) {
+  new Typewriter("#form-answer", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    display: 100,
+  });
+}
+
 function generateAnswer(event) {
   event.preventDefault();
-  
- new Typewriter("#form-answer", {
-   strings: "Hello World",
-   autoStart: true,
-   cursor: "",
-   display: 150,
- });
+
+  let instructionElement = document.querySelector("#input-prompt");
+  let apiKey = "fac344c0db5f6d5ctc9743db656e4obf";
+  let prompt = `Generate a quote with ${instructionElement.value}`;
+  let context = "You are a masterquoter AI assistent who knows all the best quotes in the world. Give the best quote using the users instructions. The quote shouldn't be more than three lines and seperated by <br />. Sign if the name of the person you quoted. No offensive and very sweet and kind quotes";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayAnswer);
 }
 
 let formElement = document.querySelector("#form-generator");
