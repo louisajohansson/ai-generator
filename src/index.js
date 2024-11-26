21426f58168c7f76c1cfbf157c5b57c61e733d9f
louisajohansson <louisa.j.johansson1@gmail.com>
2024-11-26 11:12:24 +0100

    Clarified context for AI

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ function generateAnswer(event) {
   let instructionElement = document.querySelector("#input-prompt");
   let apiKey = "fac344c0db5f6d5ctc9743db656e4obf";
   let prompt = `Generate a quote with ${instructionElement.value}`;
-  let context = "You are a masterquoter AI assistent who knows all the best quotes in the world. Give the best quote using the users instructions. The quote shouldn't be more than three lines and seperated by <br />. Sign if the name of the person you quoted. No offensive and very sweet and kind quotes";
+  let context =
+    "You are a masterquoter AI assistent who knows all the best quotes in the world. Give the best quote using the users instructions. The quote shouldn't be more than three lines and seperated by <br />. Sign if the name of the person you quoted but their name should be underneat the quote following <br />. No offensive and very sweet and kind quotes";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayAnswer);
